9cf5dc90bb364e572d1570366cf28a21e6485fa4
chungld2 <shootingstar.ldc@gmail.com>
2023-02-22 18:03:48 +0700

    Deloy ERC from Vault

--- contracts/FarmVault.sol
+++ contracts/FarmVault.sol
@@ -5,10 +5,17 @@ import "./Initializable.sol";
 import "./ERC20.sol";
 
 contract FarmVault is Initializable {
-
     ERC20UpgradeSafe tokenDebt;
 
-    constructor(string memory name,  string memory symbol) {
-        tokenDebt = new ERC20UpgradeSafe(name, symbol);
+    constructor(string memory name, string memory symbol) {
+        tokenDebt = new ERC20UpgradeSafe(string.concat("i", name), symbol);
+    }
+
+    function tokenDebtAddress() public view returns (address) {
+        return address(tokenDebt);
+    }
+
+    function vaultAddress() public view returns (address) {
+        return address(this);
     }
 }
