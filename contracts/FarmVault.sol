40fef0707e763f5e88b50aa9a210edb5d5117cd0
chungdeveloper <shootingstar.ldc@gmail.com>
2023-02-22 20:06:47 +0700

    farm vault

--- contracts/FarmVault.sol
+++ contracts/FarmVault.sol
@@ -5,7 +5,7 @@ import "./Initializable.sol";
 import "./ERC20.sol";
 
 contract FarmVault is Initializable {
-    ERC20UpgradeSafe tokenDebt;
+    ERC20UpgradeSafe private tokenDebt;
 
     constructor(string memory name, string memory symbol) {
         tokenDebt = new ERC20UpgradeSafe(string.concat("i", name), symbol);
